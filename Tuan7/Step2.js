6c955d41741e73e768a5fc44a562c790aacac973
caubebutchii <caubebutchi2k3@gmail.com>
2024-10-08 17:42:33 +0700

    update details
    render data from api

--- Tuan7/Step2.js
+++ Tuan7/Step2.js
@@ -1,294 +1,341 @@
 import React, { useEffect, useState } from 'react';
+import CheckBox from 'react-native-check-box';
 import {
-    Text,
-    SafeAreaView,
-    StyleSheet,
-    View,
-    TouchableOpacity,
-    Image,
-    FlatList,
-    ScrollView,
-    TextInput,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ScrollView,
+  TextInput,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Item = ({ item, onSelect, onUpdate, onDelete }) => (
-    <TouchableOpacity
+const todoList = [];
+const Item = ({ item }) => {
+  const [isChecked, setIsChecked] = useState(item.checked);
+  const handleCheckBoxClick = () => {
+    setIsChecked = !isChecked;
+    item.isChecked = !isChecked;
+  };
+  return (
+    <View
+      style={{
+        backgroundColor: '#DEE1E678',
+        flexDirection: 'row',
+        marginTop: 16,
+        height: 48,
+        borderRadius: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 16,
+      }}>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <CheckBox
+          style={{
+            color: '#14923E',
+          }}
+          onClick={handleCheckBoxClick}
+          isChecked={item.checked}
+        />
+        <Text style={{
+          marginLeft: 8
+        }}>{item.content}</Text>
+      </View>
+      <Image source={require('./assets/Frame(3).png')} style={{}} />
+    </View>
+  );
+};
+function HomeScreen({ navigation }) {
+  const [uname, setName] = useState('Twinkle');
+  return (
+    <View
+      style={{
+        padding: 28,
+      }}>
+      <View
         style={{
-            width: '100%',
-        }}
-        onPress={() => onSelect(item)}>
-        <View
-            style={{
-                height: 80,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                backgroundColor: item.id === 1 ? '#fff' : 'none',
-                paddingRight: 14,
-            }}>
-            <View
-                style={{
-                    justifyContent: 'flex-start',
-                    height: 80,
-                }}>
-                <Text
-                    style={{
-                        fontSize: 13,
-                        top: 4,
-                    }}>
-                    {item.name}
-                </Text>
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                    }}>
-                    <Text
-                        style={{
-                            color: 'red',
-                            top: 10,
-                            fontSize: 12,
-                            color: '#7D5B5B',
-                        }}>
-                        Price
-                    </Text>
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 82,
+        }}>
+        <Image source={require('./assets/Image95.png')} />
+      </View>
+      <Text
+        style={{
+          color: '#8353E2',
+          lineHeight: 36,
+          fontSize: 24,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginTop: 20,
+          fontStyle: 'Epilogue',
+        }}>
+        MANAGE YOUR TASK
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          marginTop: 60,
+          padding: 16,
+          borderColor: '#000',
+          borderRadius: 5,
+          borderWidth: 1,
+          height: 43,
+        }}>
+        <Image source={require('./assets/Frame.png')} />
+        <TextInput
+          onChangeText={(text) => setName(text)}
+          value={uname}
+          style={{
+            color: '#BCC1CA',
+            marginLeft: 10,
+            borderWidth: 0,
+            alignContent: 'center',
+            fontStyle: 'Epilogue',
+          }}
+          onFocus={(e) => (e.target.style.borderWidth = 0)}
+          onBlur={(e) => (e.target.style.borderWidth = 0)}
+        />
+      </View>
 
-                    <TouchableOpacity>
-                        <Text
-                            style={{
-                                color: 'red',
-                                top: 10,
-                                fontSize: 12,
-                                color: '#FF0E0E',
-                                marginLeft: 4,
-                            }}>
-                            {item.price}
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: '#F31111',
-                        width: 80,
-                        height: 34,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        marginRight: 10,
-                    }}
-                    onPress={() => onUpdate(item.id)}>
-                    <Text
-                        style={{
-                            color: '#fff',
-                        }}>
-                        Update
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: '#F31111',
-                        width: 80,
-                        height: 34,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                    onPress={() => onDelete(item.id)}>
-                    <Text
-                        style={{
-                            color: '#fff',
-                        }}>
-                        Delete
-                    </Text>
-                </TouchableOpacity>
-            </View>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#00BDD6',
+            height: 44,
+            width: 190,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 12,
+            marginTop: 100,
+          }}
+          onPress={() =>
+            navigation.navigate('Details', {
+              name: uname,
+            })
+          }>
+          <Text
+            style={{
+              fontSize: 16,
+              lineHeight: 26,
+              fontWeight: 'bold',
+              color: '#FFFFFF',
+              fontStyle: 'Epilogue',
+            }}>
+            GET STARTED ->
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+function Details({ route, navigation }) {
+  const [search, setSearch] = useState('Search');
+  const [todoList, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://670501dc031fd46a830e46b6.mockapi.io/todo')
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  return (
+    <View
+      style={{
+        padding: 18,
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <View>
+          <TouchableOpacity>
+            <Image source={require('./assets/IconButton11.png')} />
+          </TouchableOpacity>
         </View>
 
         <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <View>
+            <Image source={require('./assets/Frame11.png')} />
+          </View>
+          <View
             style={{
-                height: 1,
-                width: '100%',
-                backgroundColor: '#C4C4C4',
-            }}></View>
-    </TouchableOpacity>
-);
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginLeft: 2,
+            }}>
+            <Text
+              style={{
+                fontWeight: 700,
+                fontSize: 20,
+                color: '#171A1F',
+                lineHeight: 30,
+                textAlign: 'center',
+                fontStyle: 'Epilogue',
+                width: 110,
+              }}>
+              Hi {route.params.name ? route.params.name : ' '}
+            </Text>
+            <Text
+              style={{
+                fontWeight: 700,
+                fontSize: 14,
+                color: '#77767b',
+                lineHeight: 22,
+                textAlign: 'center',
+                fontStyle: 'Epilogue',
+              }}>
+              Have a great day a head
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          marginTop: 60,
+          padding: 16,
+          borderColor: '#9095A0',
+          borderRadius: 5,
+          borderWidth: 1,
+          height: 44,
+        }}>
+        <Image source={require('./assets/Frame(2).png')} />
+        <TextInput
+          onChangeText={(text) => setSearch(text)}
+          value={search}
+          style={{
+            color: '#BCC1CA',
+            marginLeft: 10,
+            borderWidth: 0,
+            alignContent: 'center',
+            fontStyle: 'Epilogue',
+          }}
+          onFocus={(e) => (e.target.style.borderWidth = 0)}
+          onBlur={(e) => (e.target.style.borderWidth = 0)}
+        />
+      </View>
+
+      <ScrollView>
+        <FlatList
+          data={todoList}
+          renderItem={({ item }) => <Item item={item} />}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </ScrollView>
+
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 30,
+        }}>
+        <TouchableOpacity
+          style={{
+            width: 69,
+            height: 69,
+            backgroundColor: '#00BDD6',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '50%',
+            padding: 0,
+          }}
+          onPress={() =>
+            navigation.navigate('Add', {
+              name: route.params.name,
+            })
+          }>
+          <Text
+            style={{
+              lineHeight: 69,
+              color: '#fff',
+              fontSize: 40,
+              fontWeight: 300,
+              padding: 0,
+            }}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+function Add({ route, navigation }) {
+  return (
+    <View>
+      <View>
+        <View>
+          <View>
+            <Image source={require('./assets/Frame11.png')} />
+          </View>
+          <View>
+            <Text>Hi {route.params.name}</Text>
+            <Text>Have agrate day a head</Text>
+          </View>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <Image source={require('./assets/IconButton11.png')} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState(0);
-    const [img, setImage] = useState('');
-    const [data, setData] = useState([]);
-    const [selectedItemId, setSelectedItemId] = useState(null);
-
-    const fetchData = () => {
-        fetch('https://66fd0107c3a184a84d18b0b1.mockapi.io/products2')
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data);
-            });
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const addItem = () => {
-        fetch('https://66fd0107c3a184a84d18b0b1.mockapi.io/products2', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: name,
-                price: price,
-                image: img,
-            }),
-        })
-            .then((response) => response.json())
-            .then((newItem) => {
-                setData((prevData) => [...prevData, newItem]);
-            });
-    };
-
-    const updateItem = (id) => {
-        fetch(`https://66fd0107c3a184a84d18b0b1.mockapi.io/products2/${id}`, {
-            method: 'PUT',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: name,
-                price: price,
-                image: img,
-            }),
-        })
-            .then((response) => response.json())
-            .then((updatedItem) => {
-                setData((prevData) =>
-                    prevData.map((item) => (item.id === id ? updatedItem : item))
-                );
-            });
-    };
-
-    const deleteItem = (id) => {
-        fetch(`https://66fd0107c3a184a84d18b0b1.mockapi.io/products2/${id}`, {
-            method: 'DELETE',
-        })
-            .then(() => {
-                setData((prevData) => prevData.filter((item) => item.id !== id));
-            });
-    };
-
-    const handleItemPress = (item) => {
-        setName(item.name);
-        setPrice(item.price);
-        setImage(item.image);
-        setSelectedItemId(item.id);
-    };
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <View
-                style={{
-                    margin: 10,
-                }}>
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: '#F31111',
-                        width: 100,
-                        height: 34,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                    onPress={addItem}>
-                    <Text
-                        style={{
-                            color: '#fff',
-                        }}>
-                        Add
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TextInput
-                    style={{
-                        width: 118,
-                        height: 33,
-                        marginTop: 10,
-                        backgroundColor: '#FFFFFF',
-                        fontSize: 20,
-                        fontWeight: '700',
-                        fontFamily: 'Roboto',
-                        lineHeight: 23.44,
-                        textAlign: 'right',
-                    }}
-                    value={name}
-                    onChangeText={(text) => setName(text)}
-                />
-
-                <TextInput
-                    style={{
-                        width: 118,
-                        height: 33,
-                        marginTop: 10,
-                        backgroundColor: '#FFFFFF',
-                        fontSize: 20,
-                        fontWeight: '700',
-                        fontFamily: 'Roboto',
-                        lineHeight: 23.44,
-                        textAlign: 'right',
-                    }}
-                    keyboardType="numeric"
-                    value={price.toString()}
-                    onChangeText={(text) => setPrice(Number(text))}
-                />
-
-                <TextInput
-                    style={{
-                        width: 118,
-                        height: 33,
-                        marginTop: 10,
-                        backgroundColor: '#FFFFFF',
-                        fontSize: 20,
-                        fontWeight: '700',
-                        fontFamily: 'Roboto',
-                        lineHeight: 23.44,
-                        textAlign: 'right',
-                    }}
-                    value={img}
-                    onChangeText={(text) => setImage(text)}
-                />
-            </View>
-            <ScrollView
-                style={{
-                    width: '100%',
-                }}>
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                        <Item item={item} onSelect={handleItemPress} onUpdate={updateItem} onDelete={deleteItem} />
-                    )}
-                    keyExtractor={(item) => item.id.toString()}
-                />
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <ScrollView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={Details} />
+          <Stack.Screen name="Add" component={Add} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        backgroundColor: '#ecf0f1',
-        padding: 8,
-        alignItems: 'center',
-    },
-    paragraph: {
-        margin: 24,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
