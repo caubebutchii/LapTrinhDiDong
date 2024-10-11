71070513f564254baf91c7c3d4fdf119094d8b20
H51M14\Student <Student@gmail.com>
2024-10-11 08:01:58 +0700

    main screen

--- Tuan8/App.js
+++ Tuan8/App.js
@@ -26,9 +26,10 @@ const Item = ({ item }) => {
       }}>
       <Image />
 
-      <View style={{
-        justifyContent: "space-between"
-      }}>
+      <View
+        style={{
+          justifyContent: 'space-between',
+        }}>
         <Text
           style={{
             fontWeight: 700,
@@ -69,44 +70,132 @@ const Item = ({ item }) => {
     </View>
   );
 };
-export default function App() {
+
+function HomeScreen({ route, navigation }) {
   const data = [];
   return (
     <View style={styles.container}>
-      <View style={{
-        marginTop: 10
-      }}>
-        <Text style={{
-          fontWeight: 700,
-          fontSize: 16,
-          lineHeight: 18.75,
-          opacity: 65,
-        }}>Welcome, Jala!</Text>
-        <Text style={{
-          fontWeight: 700,
-          fontSize: 20,
-          lineHeight: 23.44,
-          opacity: 65,
-          top: 6
-        }}>Choice you Best food</Text>
+      <View
+        style={{
+          marginTop: 10,
+        }}>
+        <Text
+          style={{
+            fontWeight: 700,
+            fontSize: 16,
+            lineHeight: 18.75,
+            opacity: 65,
+          }}>
+          Welcome, Jala!
+        </Text>
+        <Text
+          style={{
+            fontWeight: 700,
+            fontSize: 20,
+            lineHeight: 23.44,
+            opacity: 65,
+            top: 6,
+          }}>
+          Choice you Best food
+        </Text>
       </View>
 
-      <View>
-        <View>
-          <TextInput value={'Search food'} style={{
-            borderWidth: 1,
-
-          }}/>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        marginTop: 30,
+      }}>
+        <View style={{
+          flex: 1
+        }}>
+          <TextInput
+            value={'Search food'}
+            style={{
+              borderWidth: 1,
+              borderRadius: 3,
+              borderColor: "#000",
+              height: 40,
+              backgroundColor: "#C4C4C4",
+              opacity: "10%",
+              
+              paddingLeft: 10
+            }}
+          />
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity style={{
+          backgroundColor: "#F1B000",
+          width: 40,
+          height: 40,
+          borderRadius: 5,
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: 14,
+          marginRight: 0
+        }}>
           <Image source={require('./assets/Vector.png')} />
         </TouchableOpacity>
       </View>
 
-      <View>
+      <View style={{
+        flexDirection: "row",
+        marginTop: 24
+      }}>
         <View>
-          <TouchableOpacity>
-            <Text>Donut</Text>
+          <TouchableOpacity
+          style={{
+            height: 35,
+            borderRadius: 5,
+            borderWidth: 0,
+            justifyContent: 'center',
+            alignItems: "center",
+            backgroundColor :"#F1B000",
+            width: 101,
+          }}>
+            <Text style={{
+              color:"#0C0606",
+              fontWeight: 700,
+              fontSize: 14
+            }}>Donut</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+          style={{
+            height: 35,
+            borderRadius: 5,
+            borderWidth: 0,
+            justifyContent: 'center',
+            alignItems: "center",
+            backgroundColor :"#F1B000",
+            width: 101,
+            marginLeft: 4
+          }}>
+            <Text style={{
+              color:"#0C0606",
+              fontWeight: 700,
+              fontSize: 14
+            }}>Donut</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+          style={{
+            height: 35,
+            borderRadius: 5,
+            borderWidth: 0,
+            justifyContent: 'center',
+            alignItems: "center",
+            backgroundColor :"#F1B000",
+            width: 101,
+            marginLeft: 4
+          }}>
+            <Text style={{
+              color:"#0C0606",
+              fontWeight: 700,
+              fontSize: 14
+            }}>Donut</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -118,6 +207,21 @@ export default function App() {
           keyExtractor={(item) => item.id}
         />
       </ScrollView>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} 
+          options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
