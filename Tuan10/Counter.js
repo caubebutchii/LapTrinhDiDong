3816cfcfe5251f50c4016826838e0b8f994c6343
caubebutchii <caubebutchi2k3@gmail.com>
2024-10-29 19:48:44 +0700

    add Counter.js
    add Counter.js

---
+++ Tuan10/Counter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {View, Text, Button, StyleSheet} from 'react-native'
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './countReducer'
+
+const Counter = () => {
+  const count =useSelector((state) => state.value)
+  const dispatch = useDispatch()
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.count}>{count}</Text>
+      <Button title="increment" onPress={() => dispatch(increment())}/>
+      <br/>
+      <Button title="decrement" onPress={() => dispatch(decrement())}/>
+    </View>
+
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+export default Counter;
+
